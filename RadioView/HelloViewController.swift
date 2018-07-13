d29c66fe21d394a48c544050240a4513b0bc35f3
Mohamed Ramadan <santamo94@gmail.com>
2018-07-13 18:18:01 +0200

    initial commit

--- RadioView/HelloViewController.swift
+++ RadioView/HelloViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HelloViewController.swift
 //  RadioView
 //
 //  Created by Mo-Ramadan Abdelhafez on 7/13/18.
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HelloViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
